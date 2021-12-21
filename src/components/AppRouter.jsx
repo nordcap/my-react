bcea988a0f60b2b5a53659dba287fad92265a410
Alexey Budaev <budaevaa@mail.ru>
2021-12-21 22:53:38 +0700

    Улучшаем навигацию. Приватные и публичные маршруты

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -4,17 +4,28 @@ import About from '../pages/About';
 import Posts from '../pages/Posts';
 import Error from '../pages/Error';
 import PostIdPage from '../pages/PostIdPage';
+import Login from '../pages/Login';
 //import { routes } from '../router';
 
 
 const AppRouter = () => {
+	const isAuth = false;
+
 	return (
-		<Routes>
-			<Route path="/about" element={<About />} />
-			<Route path="/posts" element={<Posts />} />
-			<Route path="/posts/:id" element={<PostIdPage />} />
-			<Route path="*" element={<Posts />} />
-		</Routes>
+
+		isAuth
+			?
+			<Routes>
+				<Route path="/about" element={<About />} />
+				<Route path="/posts" element={<Posts />} />
+				<Route path="/posts/:id" element={<PostIdPage />} />
+				<Route path="*" element={<Posts />} />
+			</Routes >
+			:
+			<Routes>
+				<Route path="/login" element={<Login />} />
+				<Route path="*" element={<Login />} />
+			</Routes>
 	)
 }
 
