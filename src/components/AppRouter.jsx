0ee0b6f0cec698ae62e1772e0578d609a19b7a96
Alexey Budaev <budaevaa@mail.ru>
2021-12-21 22:14:34 +0700

    неудачная попытка использовать внешний файл настроек роутера

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -4,6 +4,8 @@ import About from '../pages/About';
 import Posts from '../pages/Posts';
 import Error from '../pages/Error';
 import PostIdPage from '../pages/PostIdPage';
+//import { routes } from '../router';
+
 
 const AppRouter = () => {
 	return (
@@ -11,9 +13,29 @@ const AppRouter = () => {
 			<Route path="/about" element={<About />} />
 			<Route path="/posts" element={<Posts />} />
 			<Route path="/posts/:id" element={<PostIdPage />} />
-			<Route path="*" element={<Error />} />
+			<Route path="*" element={<Posts />} />
 		</Routes>
 	)
 }
 
 export default AppRouter
+
+/*
+			1 вариант
+
+			<Route path="/about" element={<About />} />
+			<Route path="/posts" element={<Posts />} />
+			<Route path="/posts/:id" element={<PostIdPage />} />
+			<Route path="*" element={<Posts />} />
+
+
+			2 вариант
+
+			{routes.map(route =>
+				<Route
+					path={route.path}
+					element={route.element}
+				/>
+			)}
+
+*/
